93ba1555880a4b62b2146c74f54c7aa6d2d61b3a
Sean Beaudoin <s10_n@mailbox.org>
2022-05-09 22:32:25 -0700

    created recipe data structure in python

---
+++ recipes/recipe_data_structure.py
@@ -0,0 +1,12 @@
+recipe = {
+    "recipe_name": "", #string
+    "tags":["","",""], #list of strings
+    "serves": 0, #int
+    "prep_time": "", #string
+    "cook_time": "", #string
+    "image_url": "", #path to image
+    "recipe_description": "", #string
+    "ingredients": ["", ["","",""],"",["","",""]], #string for subsection, list of strings for ingredients
+    "directions": [], #list of strings
+}
+
