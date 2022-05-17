1e4ddf629039f4fc3af4286381a25760adc96918
s10 <68397573+s10-n@users.noreply.github.com>
2022-05-17 14:26:52 -0700

    recipe tags are now written to index page

--- scripts/recipe_data_structure.py
+++ scripts/recipe_data_structure.py
@@ -23,7 +23,7 @@ recipes = [
     "directions": ["Mix the dry rub ingredients in a bowl, and coat the oxtail. Once the oxtail is completely coated, dump the contents of the bowl into a plastic bag or other airtight container and refrigerate overnight.","Preheat the oven to 350. Melt the butter and olive oil in a dutch oven.","Add the oxtail and brown it on all sides. There should be a nice brown colour in the botton of the casserole dish."], #list of strings
     }, 
     {"recipe_name": "Villainous Mac & Cheeze", #string
-     "tags":["macaroni","cheese","pasta","baked","casserole"], #list of strings
+     "tags":["macaroni","cheese","pasta","baked","casserole", "stew"], #list of strings
      "serves": "4", #int
      "prep_time": "15 minutes", #string
      "cook_time": "1 hour", #string
@@ -33,26 +33,17 @@ recipes = [
      "ingredients": [["5 cups cooked macaroni (approx. 4 cups uncooked)","1 stick butter","1 cup bread crumbs","1/2 tsp salt","1/2 tsp pepper","1 tbsp sugar","1 minced fresh garlic clove","2 cups milk","1 8oz package of Colby/Monterey Jack cheese", "4 cups shredded cheddar cheese"]], #string for subsection, list of strings for ingredients
      "directions": ["Cook the macaroni until done as usual, adding sugar and garlic to water. Do not overcook it. Drain macaroni in strainer.","Place macaroni, butter, salt, pepper, milk, Colby/jack cheese, and 3 cups of cheddar cheese in an oven-safe pot. Boil slowly, and when done, add bread crumbs.","Cover everything with aluminum foil and cook for 45 minutes at 350.","Uncover and add extra cup of cheddar cheese across the top. Bake uncovered for another 15 minutes."], #list of strings
     },
-    {"recipe_name": "AAAAnother", #string
-     "tags":["macaroni","cheese","pasta","baked","casserole"], #list of strings
-     "serves": "4", #int
-     "prep_time": "15 minutes", #string
-     "cook_time": "1 hour", #string
-     "image_url": "", #path to image
-     "recipe_description": """"Macaroni and Cheese is an ol' time classic but Villainous Mac & Cheeze was concocted by wifey about three years ago. It took a couple years to perfect and you must follow the recipe exactly or else. Now Villainous Mac & Cheeze has become an MF family favorite, perfect for any holiday or special meal... enjoy and don't forget your potholderz..." - MF the Super Villain""", #string
-     "tips": "", #string
-     "ingredients": [["5 cups cooked macaroni (approx. 4 cups uncooked)","1 stick butter","1 cup bread crumbs","1/2 tsp salt","1/2 tsp pepper","1 tbsp sugar","1 minced fresh garlic clove","2 cups milk","1 8oz package of Colby/Monterey Jack cheese", "4 cups shredded cheddar cheese"]], #string for subsection, list of strings for ingredients
-     "directions": ["Cook the macaroni until done as usual, adding sugar and garlic to water. Do not overcook it. Drain macaroni in strainer.","Place macaroni, butter, salt, pepper, milk, Colby/jack cheese, and 3 cups of cheddar cheese in an oven-safe pot. Boil slowly, and when done, add bread crumbs.","Cover everything with aluminum foil and cook for 45 minutes at 350.","Uncover and add extra cup of cheddar cheese across the top. Bake uncovered for another 15 minutes."], #list of strings
-    },
-    {"recipe_name": "Recipe 3", #string
-     "tags":["macaroni","cheese","pasta","baked","casserole"], #list of strings
-     "serves": "4", #int
-     "prep_time": "15 minutes", #string
-     "cook_time": "1 hour", #string
-     "image_url": "", #path to image
-     "recipe_description": """"Macaroni and Cheese is an ol' time classic but Villainous Mac & Cheeze was concocted by wifey about three years ago. It took a couple years to perfect and you must follow the recipe exactly or else. Now Villainous Mac & Cheeze has become an MF family favorite, perfect for any holiday or special meal... enjoy and don't forget your potholderz..." - MF the Super Villain""", #string
-     "tips": "", #string
-     "ingredients": [["5 cups cooked macaroni (approx. 4 cups uncooked)","1 stick butter","1 cup bread crumbs","1/2 tsp salt","1/2 tsp pepper","1 tbsp sugar","1 minced fresh garlic clove","2 cups milk","1 8oz package of Colby/Monterey Jack cheese", "4 cups shredded cheddar cheese"]], #string for subsection, list of strings for ingredients
-     "directions": ["Cook the macaroni until done as usual, adding sugar and garlic to water. Do not overcook it. Drain macaroni in strainer.","Place macaroni, butter, salt, pepper, milk, Colby/jack cheese, and 3 cups of cheddar cheese in an oven-safe pot. Boil slowly, and when done, add bread crumbs.","Cover everything with aluminum foil and cook for 45 minutes at 350.","Uncover and add extra cup of cheddar cheese across the top. Bake uncovered for another 15 minutes."], #list of strings
-    }
+{
+    "recipe_name": "Jalapeno Cheddar Cornbread", #string
+    "tags":["cornbread","jalapeno","cheese", "southern", "baked"], #list of strings
+    "serves": "12", #int
+    "prep_time": "10 minutes", #string
+    "cook_time": "30 minutes", #string
+    "image_url": "", #path to image
+    "recipe_description": "Fantastically fluffy Jalapeño Cheddar Cornbread! This crazy good cornbread gets a leg up from two classic mix-ins: ooey gooey cheddar cheese and fiery jalapeño. The result is a kiss of heat blanketed by cheesy cornbread goodness.", #string
+    "tips": "", #string
+    "ingredients": ["", ["","",""],"",["","",""]], #string for subsection, list of strings for ingredients
+    "directions": ["","",""], #list of strings
+}    
+
 ]
--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,6 +1,10 @@
 import re
-import recipe_data_structure
+import os
 from bs4 import BeautifulSoup
+import recipe_data_structure
+
+
+
 
 # What happens when a new recipe is submitted?
 
@@ -24,7 +28,10 @@ def create_recipe_html(recipe_data): # create new HTML page
     serves = recipe_data["serves"]
     prep_time = recipe_data["prep_time"]
     cook_time = recipe_data["cook_time"]
-    image_url = recipe_data["image_url"]
+    if "image_url" in recipe_data:
+        image_url = recipe_data["image_url"]
+    else:
+        image_url = ''
     recipe_description = recipe_data["recipe_description"]
     tips = recipe_data["tips"]
     ingredients = ""
@@ -51,7 +58,7 @@ def create_recipe_html(recipe_data): # create new HTML page
         recipe_template_text=re.sub("{image_url}",image_url,recipe_template_text)
     else:
         recipe_template_text=re.sub("""<img id="image" src="{image_url}">""",image_url,recipe_template_text)
-    recipe_template_text=re.sub("{recipe_description}",rnecipe_description,recipe_template_text)
+    recipe_template_text=re.sub("{recipe_description}",recipe_description,recipe_template_text)
     if tips: # check if there are tips
         recipe_template_text=re.sub("{tips}",tips,recipe_template_text)
     else:
@@ -66,6 +73,7 @@ def create_recipe_html(recipe_data): # create new HTML page
     new_recipe_file_name = re.sub('\s', '-',f"{recipe_name}".lower()) + ".html"
     new_recipe_file = open(f"../recipes/{new_recipe_file_name}", "w")
     new_recipe_file.write(recipe_template_text)
+    print(f'Wrote {recipe_name} to {new_recipe_file_name}')
     new_recipe_file.close()
 
 # update index.html by adding a link to the new recipe under the correct header in relative alphabetical order
@@ -82,18 +90,62 @@ def rewrite_index():
     for recipe in recipe_list:
         # get the first letter of each recipe
         recipe_name = recipe["recipe_name"]
-        recipe_link = re.sub('\s', '-',f"{recipe_name}".lower()) + ".html"
+        recipe_link = re.sub('\s', '-',f"recipes/{recipe_name}".lower()) + ".html"
         first_letter = recipe_name[0].lower()
+
+        # find the correct subsection for the recipe
         navigation_section = soup.find(id=f"{first_letter}")
+
+        # create a new list item for the recipe
         recipe_li = soup.new_tag('li')
         recipe_a = soup.new_tag('a')
         recipe_a["href"] = recipe_link
         recipe_a.string = recipe_name
         recipe_li.append(recipe_a)
+
+        # add the new list item to the index page
         navigation_section.append(recipe_li)
+        print(f'Added {recipe_name} to index page')
+
+        # update tags on index page
+
+        # get the recipe's tags
+        tags = recipe["tags"]
+
+        # open the tag cloud element for the index page
+        tag_cloud = soup.find(id="tag_cloud")
+
+        # for each tag, add it to the tag cloud
+        for tag in tags:
+            if not tag_cloud.find(string=tag):
+                tag_a = soup.new_tag('a')
+                tag_a["href"] = "tags/" + tag + ".html"
+                tag_a.string = tag
+                tag_a.append(", ")
+                tag_cloud.append(tag_a)
+
+        # create a page for the recipe
+        create_recipe_html(recipe)
+
+    # sort tags alphabetically
+    tag_links = tag_cloud.find_all('a')
+    sorted_tag_links = sorted(tag_links, key=lambda elem: elem.text)
+
+    # add the sorted tags to the index page
+    for i in sorted_tag_links:
+        tag_cloud.append(i)
+
+    # remove last comma from tag cloud
+    last_tag = tag_cloud.find_all()[-1].contents
+    
+    
+    tag_cloud.find_all()[-1].string = last_tag[0]
     index_page = open("../index.html","w")
     index_page.write(str(soup))
     index_page.close()      
+rewrite_index()
+
+
 
 # for each tag associated with the recipe:
 # if a tag page exists, add the recipe to the tag page in relative alphabetical order
